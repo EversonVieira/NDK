7e2df5ba4d563df78ea5d35cd9ec87a9deee23e5
Everson Vieira Souza vieira <Everson-SV@hotmail.com>
2023-02-20 23:53:49 -0300

    2353

---
+++ NDK.Core/Utility/RandomCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDK.Core.Utility
+{
+    public static class RandomCode
+    {
+        public static string GenerateRandomCode(int length)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Random rdm = new Random();
+            for (int i = 0; i < length;)
+            {
+               sb.Append(rdm.Next(0,9));
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ NDK.Database/Base/BaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using NDK.Core.Models;
+using NDK.Core.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDK.Database.Base
+{
+    public class BaseRepository
+    {
+        public const string BASE_INSERT_SQL_COLUMNS = "IsActive, IsDeleted, CreatedBy, CreatedAt, LastUpdatedBy, LastUpdatedAt";
+        public const string BASE_INSERT_SQL_VALUES = "@IsActive, @IsDeleted, @CreatedBy, @CreatedAt, @LastUpdatedBy, @LastUpdatedAt";
+        public const string BASE_SELECT_SQL = "IsActive, IsDeleted, CreatedBy, CreatedAt, LastUpdatedBy, LastUpdatedAt";
+        public const string BASE_UPDATE_SQL = "IsActive = @IsActive, IsDeleted = @IsDeleted, CreatedBy = @CreatedBy, CreatedAt = @CreatedAt, LastUpdatedBy = @LastUpdatedBy, LastUpdatedAt = @LastUpdatedAt";
+
+        public void HandleException(Exception exception, NdkResponse response, ILogger logger)
+        {
+            logger.LogError(exception.Message, exception);
+            response.AddMessage(new NdkMessage
+            {
+                Code = $"EX{RandomCode.GenerateRandomCode(5)}_{DateTime.UtcNow}",
+                Text = "An internal error happened, try again and if the problems persists contact the support with the exception code.",
+                Type = NdkMessageType.EXCEPTION
+            });
+        }
+    }
+}
