3ca84a8f68d07c1dc6dc0f8ae457da7cc1fd8147
Everson Vieira <everson-sv@hotmail.com>
2024-02-23 20:55:03 -0300

    arara

--- NDK.Auth.Core/Interfaces/INDKTokenHandler.cs
+++ NDK.Auth.Core/Interfaces/INDKTokenHandler.cs
@@ -10,6 +10,7 @@ namespace NDK.Auth.Core.Interfaces
         Task<TToken> RetrieveTokenByStringAsync(string token);
         Task<NDKResponse<bool>> ValidateTokenAsync(TToken token);
         Task<NDKResponse<TUser>> RetrieveUserByToken(TToken token);
-
+        Task<NDKResponse<bool>> HasPermission(string permission);
+        Task<NDKResponse<bool>> HasRole(string permission);
     }
 }
