e14865070338057a2816c1359e151a72a9c01502
Everson Vieira <everson-sv@hotmail.com>
2024-03-04 02:11:47 -0300

    Fix PackAndBuild

--- PackAndBuild.ps1
+++ PackAndBuild.ps1
@@ -1,6 +1,6 @@
-Remove-Item .\nuget-packages\* -Recurse -Force
-
 dotnet pack --configuration Release --output .\nuget-packages --version-suffix (Get-Date -Format "yyMMddhhmmss")
 
 Copy-Item -Path "C:\Users\Evers\source\repos\NDK\nuget-packages\*" -Destination "C:\PrivateFeed" -Recurse -Force
+
+Remove-Item .\nuget-packages -Recurse
 
